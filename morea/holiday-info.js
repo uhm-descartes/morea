e3b96ed9cdd8364e34fc385598416a4ef899301e
Andrea Jans <57602919+awjans@users.noreply.github.com>
2025-07-03 06:15:28 -1000

    Create holiday-info.js

---
+++ morea/holiday-info.js
@@ -0,0 +1,6 @@
+var holidayData = {color: 'green',
+  events: [
+    {title: "Classes start", start: "2021-07-06"},
+    {title: "Holiday: Independence Day (Observed)", start: "2021-07-05"},
+    {title: "Classes end", start: "2021-07-30"},
+  ]};
